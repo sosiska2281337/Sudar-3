1e3623eaf4618cbcb0baa445fa2681bd5ceca3ac
sosiska2281337 <danhoh22895@gmail.com>
2016-11-26 16:07:12 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include "TXLib.h"
 
-//Рисование стойки бара верхней
-void stand()
+//ГђГЁГ±Г®ГўГ Г­ГЁГҐ Г±ГІГ®Г©ГЄГЁ ГЎГ Г°Г  ГўГҐГ°ГµГ­ГҐ
+void drawBar()
 {
-    while (true)
-    {
-        txSetColour(TX_GREEN);
+    txSetColour(TX_GREEN);
 
-        txLine(600, 100, 600, 50);
-        txLine(600, 50, 750,  50);
-        txLine(750, 100, 750, 50);
-        txLine(600, 100, 750, 100);
-        txLine(638, 50,  638, 2);
-        txLine(710, 50,  710, 2);
+    txLine(600, 100, 600, 50);
+    txLine(600, 50, 750,  50);
+    txLine(750, 100, 750, 50);
+    txLine(600, 100, 750, 100);
+    txLine(638, 50,  638, 2);
+    txLine(710, 50,  710, 2);
 
-        txSetColour(RGB(random(255),random(255),random(255)));
-        txSelectFont ("Comic Sans MS", 80) ;
-        txTextOut (625, 33, "BAR");
-        txSleep(100);
-    }
+    txSetColour(RGB(random(255),random(255),random(255)));
+    txSelectFont ("Comic Sans MS", 80) ;
+    txTextOut (625, 33, "BAR");
+    txSleep(100);
 }
 
 void butulki(int x, int y)
@@ -43,87 +40,94 @@ void drawStoika(int x, int y)
     txLine(x-998,  y-130,  x, y-130);
     txLine(x-998,  y, x, y);
 }
+
 void drawSudar(int x, int y, int polojenie_nogi)
 {
     txSetColour(TX_RED, 5);
-    // Ноги
+    // ГЌГ®ГЈГЁ
     txLine(x+30, y+160, x+30, y+70);
     txLine(x+30+polojenie_nogi, y+160, x+30, y+70);
-    // Тело
+    // Г’ГҐГ«Г®
     txLine(x+30, y, x+30, y+70);
     txLine(x+30, y, x+30, y+40);
     txCircle(x+30, y-20, 14);
-    // Ручки
+    // ГђГіГ·ГЄГЁ
     txLine(x+10, y+60, x+30, y);
     txLine(x+60, y+35, x+30, y);
 }
 
 /*
-Добавить зависимость от x, y
-Добавить параметр - наличие бутылки в руке
+Г„Г®ГЎГ ГўГЁГІГј Г§Г ГўГЁГ±ГЁГ¬Г®Г±ГІГј Г®ГІ x, y
+Г„Г®ГЎГ ГўГЁГІГј ГЇГ Г°Г Г¬ГҐГІГ° - Г­Г Г«ГЁГ·ГЁГҐ ГЎГіГІГ»Г«ГЄГЁ Гў Г°ГіГЄГҐ
 */
-void drawBomj(int x, int y)
+void drawBomj (int x, int y)
 {
-    // Человек 2
-    // Ноги
+    // Г—ГҐГ«Г®ГўГҐГЄ 2
+    // ГЌГ®ГЈГЁ
     txSetColour(TX_GREEN, 5);
     txLine(650, 560, 670, 470);
     txLine(700, 560, 670, 470);
-    // Тело
+    // Г’ГҐГ«Г®
     txLine(670, 470, 670, 400);
     txLine(660, 390, 670, 400);
     txCircle(660, 376, 14);
-    // Ручки
+    // ГђГіГ·ГЄГЁ
     txLine(670, 400, 650, 415);
     txLine(650, 415, 630, 405);
 
     txLine(670, 400, 685, 430);
     txLine(685, 430, 685, 470);
+}
 
+void drawShkaff(int x, int y)
+{
+    txSetColour(TX_BROWN);
+    txLine(x,  y+150, x,     y- 50);
+    txLine(x,  y+100, x+390, y+100);
+    txLine(x,  y+ 50, x+390, y+ 50);
+    txLine(x,  y,     x+390, y);
+    txLine(x,  y- 50, x+390, y- 50);
+    txLine(x+390,y+150,x+390,y- 50);
+}
+
+void poyavlenieButylok (int zaderjka)
+{
+    for (int ryad = 1; ryad <= 4; ryad++) {
+        for (int pos = 1; pos <= 8; pos++) {
+            butulki(50*pos, 370-50*ryad);
+
+            if (zaderjka > 0) {
+                txSleep(zaderjka);
+            }
+        }
+    }
+}
+void nuzhnoe()
+{
+
+    drawStoika(1000, 550);
+    drawBomj(670, 400);
+    drawShkaff(10, 200);
+    drawBar();
+    poyavlenieButylok(100);
 }
 
 int main()
 {
     txCreateWindow(1000, 800);
-    drawStoika(1000, 550);
-
-    /*Создать отдельную функцию появления бутылок*/
-    for (int ryad = 1; ryad <= 4; ryad++) {
-        for (int pos = 1; pos <= 8; pos++) {
-            butulki(50*pos, 370-50*ryad);
-            txSleep(100);
-        }
-    }
-
-    for (int i = 1; i <= 40; i++) {
+    nuzhnoe();
+    for (int i = 3; i <= 40; i++) {
         txSetFillColour(TX_BLACK);
         txClear();
 
-        for (int ryad = 1; ryad <= 4; ryad++) {
-            for (int pos = 1; pos <= 8; pos++) {
-                butulki(50*pos, 370-50*ryad);
-            }
-        }
-        //i%2 = 0 для четных, 1 для нечетных
-        //40 и -40 поэтому 40-80*(i%2)
+        poyavlenieButylok(0);
         drawSudar(320+10*i, 400, 40 - 80*(i%2));
+        drawShkaff(10, 200);
+        drawBomj(670, 400);SS
+        drawBar();
 
-        drawBomj(670, 400);
-        // Шкаф
-
-        /*Создать отдельную функцию рисования шкафа, сделать цикл*/
-        txSetColour(TX_BROWN);
-        txLine(10, 350, 10,  150);
-        txLine(10, 300, 400, 300);
-        txLine(10, 250, 400, 250);
-        txLine(10, 200, 400, 200);
-        txLine(10, 150, 400, 150);
-        txLine(400, 350,400,150);
-
-        txSleep(100);
+        txSleep(30);
     }
 
-    stand();
     return 0;
 }
-
