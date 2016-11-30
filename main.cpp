6e385ee2f96657913dc7e73572acca8316fb5c0f
sosiska2281337 <danhoh22895@gmail.com>
2016-11-30 17:11:17 +0400

    AZAZAAZAZAZAA

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include "TXLib.h"
 
+/*
+АЗАЗАЗАЗАЗАЗАЗАЗЗААЗАЗЗАЗАЗАЗАЗАЗАЗЗАЗАЗАЗАЗАЗАЗАЗААЗАЗАЗАЗАЗЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗАЗА
+*/
 //Ðèñîâàíèå ñòîéêè áàðà âåðõíå
-void drawBar()
+void drawBar(int x, int y)
 {
     txSetColour(TX_GREEN);
 
-    txLine(600, 100, 600, 50);
-    txLine(600, 50, 750,  50);
-    txLine(750, 100, 750, 50);
-    txLine(600, 100, 750, 100);
-    txLine(638, 50,  638, 2);
-    txLine(710, 50,  710, 2);
+    txLine(x,   y, x, y-50);
+    txLine(x,   y-50,  x+150, y-50);
+    txLine(x+150, y, x+150, y-50);
+    txLine(x,   y, x+150, y);
+    txLine(x+38, y-50,  x+38, y-598);
+    txLine(x+110, y-50, x+110 , y-598);
 
     txSetColour(RGB(random(255),random(255),random(255)));
     txSelectFont ("Comic Sans MS", 80) ;
     txTextOut (625, 33, "BAR");
-    txSleep(100);
+    //txSleep(100);
 }
 
 void butulki(int x, int y)
@@ -65,23 +68,23 @@ void drawBomj (int x, int y)
     // ×åëîâåê 2
     // Íîãè
     txSetColour(TX_GREEN, 5);
-    txLine(650, 560, 670, 470);
-    txLine(700, 560, 670, 470);
+    txLine(x-20, y+160, x, y+70);
+    txLine(x+100, y+160, x, y+70);
     // Òåëî
-    txLine(670, 470, 670, 400);
-    txLine(660, 390, 670, 400);
-    txCircle(660, 376, 14);
+    txLine(x, y+70, x, y);
+    txLine(x-10, y-10, x, y);
+    txCircle(x-10, y-24, 14);
     // Ðó÷êè
-    txLine(670, 400, 650, 415);
-    txLine(650, 415, 630, 405);
+    txLine(x, y, x-20, y+15);
+    txLine(x-20, y+15, x-40, y+5);
 
-    txLine(670, 400, 685, 430);
-    txLine(685, 430, 685, 470);
+    txLine(x, y,     x+15, y+30);
+    txLine(x+15, y+30, x+15, y+70);
 }
 
 void drawShkaff(int x, int y)
 {
-    txSetColour(TX_BROWN);
+    txSetColour(TX_WHITE);
     txLine(x,  y+150, x,     y- 50);
     txLine(x,  y+100, x+390, y+100);
     txLine(x,  y+ 50, x+390, y+ 50);
@@ -102,32 +105,61 @@ void poyavlenieButylok (int zaderjka)
         }
     }
 }
-void nuzhnoe()
-{
 
-    drawStoika(1000, 550);
-    drawBomj(670, 400);
-    drawShkaff(10, 200);
-    drawBar();
-    poyavlenieButylok(100);
-}
 
 int main()
 {
     txCreateWindow(1000, 800);
-    nuzhnoe();
-    for (int i = 3; i <= 40; i++) {
+
+    int i=1;
+    for (i = 1; i <= 22; i++) {
         txSetFillColour(TX_BLACK);
         txClear();
 
         poyavlenieButylok(0);
         drawSudar(320+10*i, 400, 40 - 80*(i%2));
         drawShkaff(10, 200);
-        drawBomj(670, 400);SS
-        drawBar();
+        drawBomj(670, 400);
+        drawBar(600, 100);
+        drawStoika(1000, 550);
 
         txSleep(30);
     }
+    //you are
+    for (i = 3; i <= 22; i++) {
+        txSetFillColour(TX_BLACK);
+        txClear();
+
+        poyavlenieButylok(0);
+        drawBomj(670, 400);
+        drawBar(600, 100);
+        drawStoika(1000, 550);
+        drawSudar(540, 400, - 40);
+        drawShkaff(10, 200);
+
+        txSetColour(TX_YELLOW);
+        txSelectFont ("Times New Roman", 20) ;
+        txTextOut (600, 300, "You are ");
+        txSleep(30);
+
+    }
+
+   //idiot
+    txSetFillColour(TX_BLACK);
+    txClear();
+
+    poyavlenieButylok(0);
+    drawBomj(670, 400);
+    drawBar(600, 100);
+    drawStoika(1000, 550);
+    drawSudar(540, 400, 40);
+    drawShkaff(10, 200);
+
+    txSetColour(TX_YELLOW);
+    txSelectFont ("Times New Roman", 40) ;
+    txTextOut (700, 300, "IDIOT!");
 
     return 0;
 }
+
+
